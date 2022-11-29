0eb32aac3d83dd814db1964d2582fd6340d3cc67
LuisGomez24 <luisfergs24@gmail.com>
2022-11-28 22:55:41 -0600

    Predictor Bimodal Completado

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 # Copyright 2022 - Luis Fernando Gomez Sanchez - luis.gomez20@ucr.ac.cr
 import argparse
+import linecache
+from predictors.bimodal import BimodalPredictor
 
-def BimodalPredictor(branch_options) -> dict:
+def start_bimodal_prediction(index_size) -> dict:
     simulation_result = {
         'branches':            1,
         'correctly_taken':     0,
@@ -9,6 +11,22 @@ def BimodalPredictor(branch_options) -> dict:
         'correctly_untaken':   0,
         'incorrectly_untaken': 0,
     }
+    
+    predictor = BimodalPredictor(index_size)
+    chunk = 1
+    while True:
+        trace_line = get_PC_address(chunk)
+        if not trace_line:
+            break
+        
+        branch_data = trace_line.split()
+        pc_address = int(branch_data[0])
+        taken = True if branch_data[1] == "T" else False
+        
+        simulation_result = predictor.predict_branch(simulation_result,
+                                                     pc_address,
+                                                     taken)
+        chunk += 1
     
     return simulation_result
 
@@ -36,34 +54,31 @@ def GlobalPredictor(branch_options) -> dict:
     
     return simulation_result
 
+def get_PC_address(index) -> str:
+    return linecache.getline('../trace/trace.txt', index)
 
-def TournamentPredictor(branch_options) -> dict:
-    simulation_result = {
-        'branches':            1,
-        'correctly_taken':     0,
-        'incorrectly_taken':   0,
-        'correctly_untaken':   0,
-        'incorrectly_untaken': 0,
-    }
-    
-    return simulation_result
-
+def print_predictor_params(predictor_type, predictor_params) -> None:
+    print("Parámetros del predictor:")
+    print(f"\tTipo de predictor: {predictor_type}")
+    print(f"\tEntradas en el predictor: {2 ** predictor_params.IndexSize}")
 
 def print_simulation_result(simulation_result) -> None:
-    prediction_percent = ((simulation_result["correctly_taken"] + 
-                          simulation_result["correctly_untaken"]) / 
-                          simulation_result["branches"])
+    prediction_percent = ((simulation_result['correctly_taken'] + 
+                          simulation_result['correctly_untaken']) / 
+                          simulation_result['branches'])
+    prediction_percent = round(prediction_percent * 100, 4)
     
-    print(f'# branches: {simulation_result["branches"]}')
-    print('# branches tomados predichos correctamente: '      +
-        str(simulation_result["correctly_taken"]))
-    print('# branches tomados predichos incorrectamente: '    +
-        str(simulation_result["incorrectly_taken"]))
-    print('# branches no tomados predichos correctamente: '   +
-        str(simulation_result["correctly_untaken"]))
-    print('# branches no tomados predichos incorrectamente: ' +
-        str(simulation_result["incorrectly_untaken"]))
-    print(f'% predicciones correctas: {prediction_percent}')
+    print("Resultados de la simulación:")
+    print(f"\t# branches: {simulation_result['branches']}")
+    print('\t# branches tomados predichos correctamente: '      +
+        str(simulation_result['correctly_taken']))
+    print('\t# branches tomados predichos incorrectamente: '    +
+        str(simulation_result['incorrectly_taken']))
+    print('\t# branches no tomados predichos correctamente: '   +
+        str(simulation_result['correctly_untaken']))
+    print('\t# branches no tomados predichos incorrectamente: ' +
+        str(simulation_result['incorrectly_untaken']))
+    print(f'\t% predicciones correctas: {prediction_percent}%')
 
 
 def main() -> None:
@@ -74,22 +89,22 @@ def main() -> None:
         help = "Cantidad de Bits para Indexar.", type=int, required=True)
     parser.add_argument("-bp", "--BranchPredictor",
         help = "Tipo de Predictor. Opciones: 0: Bimodal 2 bits \
-        / 1: Global 2 Niveles / 2:Local 2 Niveles / 3: Torneo",
-        default=0, type=int, choices=range(0,4))
+        / 1: Global 2 Niveles / 2:Local 2 Niveles",
+        default=0, type=int, choices=range(0,4), required=True)
     parser.add_argument("-gh", "--GlobalHistory",
         help = "Tamaño de Registro de Historia Global.", default=0, type=int)
     parser.add_argument("-lh", "--LocalHistory",
         help = "Tamaño de Registro de Historia Local.", default=0, type=int)
 
     args = parser.parse_args()
+    
     if args.BranchPredictor == 0:
-        simulation_result = BimodalPredictor(args)
+        print_predictor_params("Bimodal", args)
+        simulation_result = start_bimodal_prediction(args.IndexSize)
     elif args.BranchPredictor == 1:
         simulation_result = GlobalPredictor(args)
-    elif args.BranchPredictor == 2:
-        simulation_result = LocalPredictor(args)
     else:
-        simulation_result = TournamentPredictor(args)
+        simulation_result = LocalPredictor(args)
     
     print_simulation_result(simulation_result)
 
