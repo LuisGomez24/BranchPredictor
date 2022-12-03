89392db135ad341213a69e05dbc5fa132a54e1e4
LuisGomez24 <luisfergs24@gmail.com>
2022-12-02 22:47:03 -0600

    Main general para ejecucion del programa

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 # Copyright 2022 - Luis Fernando Gomez Sanchez - luis.gomez20@ucr.ac.cr
 import argparse
 import linecache
-from predictors.bimodal import BimodalPredictor
+from predictors import BimodalPredictor
+from predictors import GlobalPredictor
 
 def start_bimodal_prediction(index_size) -> dict:
     simulation_result = {
-        'branches':            1,
+        'branches':            0,
         'correctly_taken':     0,
         'incorrectly_taken':   0,
         'correctly_untaken':   0,
@@ -30,27 +31,30 @@ def start_bimodal_prediction(index_size) -> dict:
     
     return simulation_result
 
-
-def LocalPredictor(branch_options) -> dict:
+def start_global_prediction(index_size, global_size) -> dict:
     simulation_result = {
-        'branches':            1,
+        'branches':            0,
         'correctly_taken':     0,
         'incorrectly_taken':   0,
         'correctly_untaken':   0,
         'incorrectly_untaken': 0,
     }
     
-    return simulation_result
-
-
-def GlobalPredictor(branch_options) -> dict:
-    simulation_result = {
-        'branches':            1,
-        'correctly_taken':     0,
-        'incorrectly_taken':   0,
-        'correctly_untaken':   0,
-        'incorrectly_untaken': 0,
-    }
+    predictor = GlobalPredictor(index_size, global_size)
+    chunk = 1
+    while True:
+        trace_line = get_PC_address(chunk)
+        if not trace_line:
+            break
+        
+        branch_data = trace_line.split()
+        pc_address = int(branch_data[0])
+        taken = True if branch_data[1] == "T" else False
+        
+        simulation_result = predictor.predict_branch(simulation_result,
+                                                     pc_address,
+                                                     taken)
+        chunk += 1
     
     return simulation_result
 
@@ -61,6 +65,8 @@ def print_predictor_params(predictor_type, predictor_params) -> None:
     print("Parámetros del predictor:")
     print(f"\tTipo de predictor: {predictor_type}")
     print(f"\tEntradas en el predictor: {2 ** predictor_params.IndexSize}")
+    if (predictor_type == "Global"):
+        print(f"\tTamaño de los registros de historia global: {predictor_params.GlobalHistory}")
 
 def print_simulation_result(simulation_result) -> None:
     prediction_percent = ((simulation_result['correctly_taken'] + 
@@ -89,23 +95,29 @@ def main() -> None:
         help = "Cantidad de Bits para Indexar.", type=int, required=True)
     parser.add_argument("-bp", "--BranchPredictor",
         help = "Tipo de Predictor. Opciones: 0: Bimodal 2 bits \
-        / 1: Global 2 Niveles / 2:Local 2 Niveles",
-        default=0, type=int, choices=range(0,4), required=True)
+        / 1: Global 2 Niveles",
+        default=0, type=int, choices=range(0,2), required=True)
     parser.add_argument("-gh", "--GlobalHistory",
         help = "Tamaño de Registro de Historia Global.", default=0, type=int)
-    parser.add_argument("-lh", "--LocalHistory",
-        help = "Tamaño de Registro de Historia Local.", default=0, type=int)
 
     args = parser.parse_args()
     
-    if args.BranchPredictor == 0:
+    if (args.BranchPredictor == 0):
         print_predictor_params("Bimodal", args)
         simulation_result = start_bimodal_prediction(args.IndexSize)
+    elif args.BranchPredictor == 1 and args.GlobalHistory == 0:
+        print("usage: main.py [-h] -s INDEXSIZE -bp {0,1} [-gh GLOBALHISTORY]")
+        print("main.py: error: the following arguments are required: -gh/--GlobalHistory")
+        exit(0)
     elif args.BranchPredictor == 1:
-        simulation_result = GlobalPredictor(args)
-    else:
-        simulation_result = LocalPredictor(args)
-    
+        print_predictor_params("Global", args)
+        if args.IndexSize < args.GlobalHistory:
+            simulation_result = start_global_prediction(args.IndexSize, 
+                                                        args.IndexSize)
+        else:
+            simulation_result = start_global_prediction(args.IndexSize,
+                                                        args.GlobalHistory)
+        
     print_simulation_result(simulation_result)
 
 if __name__ == '__main__':
